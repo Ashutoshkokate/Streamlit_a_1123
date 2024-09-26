d9e8eef2e1a04316ff81249e1fc426416f194cf8
Ashutoshkokate <100461338+Ashutoshkokate@users.noreply.github.com>
2024-09-26 12:15:03 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title('Streamlit Application')
+st.write('This is My Application fot Streamlit')
+st.image('streamlit_img.png')
+
+if st.button("Wahii !!!"):
+    st.balloons()
+    st.image('wel.jpg')
